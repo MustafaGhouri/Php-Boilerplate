ccc0c5b567129835091824ef8c9918d9aaf89dff
MustafaGhouri <mustafaghouri22@gmail.com>
2023-08-27 17:13:06 +0500

    remove extra data

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php include 'include/header.php'?>
+<?php include 'include/header.php' ?>
 
 <section class='home_section'>
-  
+    <center>Welcome to Nexu Solutions php boilerplate</center>
 </section>
-<?php include 'include/footer.php'?>
+<?php include 'include/footer.php' ?>
